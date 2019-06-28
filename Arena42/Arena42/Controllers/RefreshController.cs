bde5cceba489545ee7ae84bf97de53b90451b059
Mathieu RAES <m.raes@betclicgroup.com>
2019-06-28 10:57:32 +0200

    sportwebapi

---
+++ Arena42/Arena42/Controllers/RefreshController.cs
@@ -0,0 +1,21 @@
+﻿using Arena42.Services;
+using Swashbuckle.Swagger.Annotations;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Arena42.Controllers
+{
+    public class RefreshController : ApiController
+    {
+        [Route("api/refresh")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        public async Task<IHttpActionResult> Refresh()
+        {
+            var service = new SportWebApiService();
+            await service.GetMarkets();
+
+            return Ok();
+        }
+    }
+}
--- Arena42/Arena42/Services/SportWebApiService.cs
+++ Arena42/Arena42/Services/SportWebApiService.cs
@@ -4,11 +4,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Arena42.Services
 {
     public class SportWebApiService
     {
+        public async Task<IEnumerable<Market>> GetMarkets()
+        {
+            string token;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://globalapi.begmedia.com/");
+                var response = await client.PostAsJsonAsync("api/pub/handshake", new {
+                    channel= "BetclicComIphoneApp",
+                    universe= "Sports",
+                    languageCode= "Fr",
+                    sitecode= "FrFr"
+                });
+                var deserializedResponse = await response.Content.ReadAsJsonAsync<GlobalTokenResponse>();
+                token = deserializedResponse.Token;
+            }
 
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://sportapi.begmedia.com/api/pub/v3/");
+                client.
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var response = await client.GetAsync("events");
+            }
+
+            return null;
+        }
     }
 }
