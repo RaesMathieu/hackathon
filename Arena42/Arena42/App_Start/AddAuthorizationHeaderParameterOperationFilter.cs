a2fbd803a7080622c63b4ad59ea571f701b6faae
ocaroff <ocaroff@betclicgroup.com>
2019-06-28 10:54:54 +0200

    login + Gestion user

---
+++ Arena42/Arena42/App_Start/AddAuthorizationHeaderParameterOperationFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace Arena42.App_Start
+{
+    public class AddAuthorizationHeaderParameterOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters != null)
+            {
+                operation.parameters.Add(new Parameter
+                {
+                    name = "X-CLIENT",
+                    @in = "header",
+                    description = "access token",
+                    required = false,
+                    type = "string"
+                });
+            }
+        }
+    }
+}
---
+++ Arena42/Arena42/Services/EF/Configuration/UserMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Arena42.Models;
+
+namespace Arena42.Services.EF.Configuration
+{
+    internal class UserMapping : EntityTypeConfiguration<User>
+    {
+        public UserMapping()
+        {
+            ToTable("User");
+            HasKey(g => g.Id);
+
+            Property(g => g.UserName).IsRequired();
+            Property(g => g.Password).IsOptional();
+        }
+    }
+}
